98bbb12395aebec9dfeb300204aa12528b6d148a
David Miller <david@ironsummitmedia.com>
2015-01-25 16:50:53 -0500

    Update Date

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,6 +1,6 @@
 /*!
  * Clean Blog v1.0.0 (http://startbootstrap.com)
- * Copyright 2014 Start Bootstrap
+ * Copyright 2015 Start Bootstrap
  * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
  */
 
