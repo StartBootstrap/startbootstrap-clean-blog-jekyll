80585d55da80c3ae2e56a5ec000faaf178c5a90c
Milan Aryal <considermilan@gmail.com>
2015-01-27 21:47:16 +0545

    Responsive tables and embed videos
    Wrap and add extra classes of Bootstrap CSS to have responsive table and embed YouTube and Vimeo 16 by 9 videos.

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -997,8 +997,24 @@ $('#name').focus(function() {
 
 })(jQuery);
 
-//make all images responsive
-$("img").addClass("img-responsive")
+// make all images responsive
+$(function() {
+	$("img").addClass("img-responsive");
+});
+
+// responsive tables
+$(document).ready(function() {
+	$("table").wrap("<div class='table-responsive'></div>");
+	$("table").addClass("table");
+});
+
+// responsive embed videos
+$(document).ready(function () { 
+    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+});
 
 // Floating label headings for the contact form
 $(function() {
