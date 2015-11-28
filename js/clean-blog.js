5646062f418967c70f42b0cd86d2f2487ac82335
David Miller <david@ironsummitmedia.com>
2015-11-28 13:18:24 -0500

    Bootstrap v3.3.6 Update

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -13,7 +13,7 @@ $(function() {
 
 $(function() {
 
-    $("input,textarea").jqBootstrapValidation({
+    $("#contactFrom input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
