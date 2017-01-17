494cc41ce355706f387639492b6e78ad0829ca22
Kshiraja Krishnan <kshiraja@fieldsofview.in>
2017-01-17 12:43:03 +0530

    Changes to css.

--- js/clean-blog.min.js
+++ js/clean-blog.min.js
@@ -1,6 +1,6 @@
 /*!
  * Clean Blog v1.0.0 (http://startbootstrap.com)
- * Copyright 2016 Start Bootstrap
+ * Copyright 2017 Start Bootstrap
  * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
  */
 
