ddfcfd6f30bc0ed7694c73eee8315a3a8c12eb1d
David Miller <hello@davidmiller.io>
2019-02-01 01:06:19 -0500

    update bootstrap version and dependencies

--- README.md
+++ README.md
@@ -139,4 +139,4 @@ Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework 
 
 ## Copyright and License
 
-Copyright 2013-2018 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-clean-blog-jekyll/blob/gh-pages/LICENSE) license.
+Copyright 2013-2019 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-clean-blog-jekyll/blob/gh-pages/LICENSE) license.
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,45 @@
-var gulp = require('gulp');
+// Load plugins
+const gulp = require("gulp");
 
-gulp.task('copy', function() {
+// Copy third party libraries from /node_modules into /vendor
+gulp.task('vendor', function(cb) {
 
   // Start Bootstrap Clean Blog SCSS
-  gulp.src(['node_modules/startbootstrap-clean-blog/scss/**/*'])
-    .pipe(gulp.dest('assets/vendor/startbootstrap-clean-blog/scss'))
+  gulp.src([
+      './node_modules/startbootstrap-clean-blog/scss/**/*'
+    ])
+    .pipe(gulp.dest('./assets/vendor/startbootstrap-clean-blog/scss'))
 
   // Start Bootstrap Clean Blog JS
   gulp.src([
-      'node_modules/startbootstrap-clean-blog/js/clean-blog.min.js',
-      'node_modules/startbootstrap-clean-blog/js/jqBootstrapValidation.js'
+      './node_modules/startbootstrap-clean-blog/js/clean-blog.min.js',
+      './node_modules/startbootstrap-clean-blog/js/jqBootstrapValidation.js'
     ])
-    .pipe(gulp.dest('assets/vendor/startbootstrap-clean-blog/js'))
+    .pipe(gulp.dest('./assets/vendor/startbootstrap-clean-blog/js'))
 
   // Bootstrap
   gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
     ])
-    .pipe(gulp.dest('assets/vendor/bootstrap'))
+    .pipe(gulp.dest('./assets/vendor/bootstrap'))
 
   // jQuery
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('assets/vendor/jquery'))
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./assets/vendor/jquery'))
 
   // Font Awesome
   gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
+      './node_modules/@fortawesome/**/*',
     ])
-    .pipe(gulp.dest('assets/vendor/font-awesome'))
+    .pipe(gulp.dest('./assets/vendor'))
 
-})
+  cb();
 
-// Default task
-gulp.task('default', ['copy']);
+});
+
+gulp.task("default", gulp.parallel('vendor'));
